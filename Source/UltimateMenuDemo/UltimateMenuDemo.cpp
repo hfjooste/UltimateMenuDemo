369b125d033f9d636702b157ceec29f20f53f751
Henry Jooste <henry@nyamakop.co.za>
2025-05-31 23:24:33 +0200

    Initial commit

---
+++ Source/UltimateMenuDemo/UltimateMenuDemo.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UltimateMenuDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UltimateMenuDemo, "UltimateMenuDemo" );
---
+++ Source/UltimateMenuDemo/UltimateMenuDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UltimateMenuDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ULTIMATEMENUDEMO_API AUltimateMenuDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
